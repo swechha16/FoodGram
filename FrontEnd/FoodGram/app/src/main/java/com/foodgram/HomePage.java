6ba7b8047bb364ae173791b39a9cef497e980f5b
amcordts <amcordts@iastate.edu>
2019-09-15 18:32:34 -0500

    Formatting Home pagge

--- FrontEnd/FoodGram/app/src/main/java/com/foodgram/HomePage.java
+++ FrontEnd/FoodGram/app/src/main/java/com/foodgram/HomePage.java
@@ -2,12 +2,27 @@ package com.foodgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button welcome_login;
+    Button welcome_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        welcome_login = findViewById(R.id.loginButton);
+        welcome_signin = findViewById(R.id.SignUpButton);
     }
+
+    welcome_login.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v)
+        // Code here executes on main thread after user presses button
+    });
+
+
+
 }
