cfab180bdbcdc5712bfadae23434870cea5606d4
amcordts <amcordts@iastate.edu>
2019-10-21 00:55:05 -0500

    POST for new USER working...no check if user exist or if information is valid

--- BackEnd/FoodGram/src/main/java/com/FoodGramServer/FoodGramServer/controllers/UserController.java
+++ BackEnd/FoodGram/src/main/java/com/FoodGramServer/FoodGramServer/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.FoodGramServer.FoodGramServer.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.FoodGramServer.FoodGramServer.models.Photo;
 import com.FoodGramServer.FoodGramServer.models.User;
 import com.FoodGramServer.FoodGramServer.repo.UserRepo;
 
@@ -15,25 +17,26 @@ public class UserController {
 
 	UserRepo userRepo;
 
-	//returns all users in the User table
-	
+	// returns all users in the User table
+
 	@RequestMapping(method = RequestMethod.GET, path = "/user/all")
-	public User[] getComments() { 
+	public User[] getComments() {
 
 		User[] user = userRepo.getAll();
-		return user; 
-		}
-
-	
-	
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/user/{username}")
-	public User[] getUserByUser(@PathVariable String username) { 
-
-		User[] user = userRepo.getByUsername(username);
-		return user; }
-	
-
-	
+		return user;
 	}
 
+	@RequestMapping(method = RequestMethod.GET, path = "/user/{username}")
+	public User[] getUserByUser(@PathVariable String username) {
+
+		User[] user = userRepo.getByUsername(username);
+		return user;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, path = "/post/user")
+	public void createNewUser(@RequestBody User newUser) {
+		userRepo.save(newUser);
+
+	}
+
+}
