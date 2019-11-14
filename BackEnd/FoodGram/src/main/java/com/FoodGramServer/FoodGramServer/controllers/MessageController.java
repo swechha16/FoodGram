8b3f28bc498525b860c095ce7c87ade6bb4d6100
Swechha Ghimire <swechha@sanjays-air-2.student.iastate.edu>
2019-11-13 23:34:42 -0600

    message db created

--- BackEnd/FoodGram/src/main/java/com/FoodGramServer/FoodGramServer/controllers/MessageController.java
+++ BackEnd/FoodGram/src/main/java/com/FoodGramServer/FoodGramServer/controllers/MessageController.java
@@ -2,6 +2,7 @@ package com.FoodGramServer.FoodGramServer.controllers;
 
 import com.FoodGramServer.FoodGramServer.models.Message;
 import com.FoodGramServer.FoodGramServer.models.Photo;
+import com.FoodGramServer.FoodGramServer.models.User;
 import com.FoodGramServer.FoodGramServer.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +14,22 @@ public class MessageController {
     MessageRepo messageRepo;
 
 
-    @RequestMapping(method = RequestMethod.POST, path = "/websocket/post/{photo}")
+    @RequestMapping(method = RequestMethod.POST, path = "/websocket/post/{message}")
     public void postWebSocketChat(@PathVariable Message message) {
-        messageRepo.save(message);
+    Message m = new Message(); 
+    m.setId_message(message.getId_message());
+    m.setMessage(message.getMessage());
+    m.setTimestamp(message.getTimestamp());
+    m.setUser1(message.getUser1()); 
+    m.setUser2(message.getUser2());
+
 
 
 
     }
-    @RequestMapping(method = RequestMethod.GET, path = "/websocket/get")
-    public Message[] getWebSocketChat(@PathVariable Message message) {
-        Message[] mess = messageRepo.getAll();
+    @RequestMapping(method = RequestMethod.GET, path = "/websocket/get/{message}/{user1}/{user2}")
+    public Message[] getWebSocketChat(@PathVariable Message message, @PathVariable User user1,  @PathVariable User user2) {
+        Message[] mess = messageRepo.getTwoUsersFromMessage(user1, user2);
         return mess;
     }
 
