d44a0a7a663e88ec4f04692b44f9e5c1dfba9fb5
Swechha Ghimire <swechha@sanjays-air-2.student.iastate.edu>
2019-11-20 15:34:54 -0600

    refresh button is working

--- FrontEnd/FoodGram/app/src/main/java/com/foodgram/RestaurantPage.java
+++ FrontEnd/FoodGram/app/src/main/java/com/foodgram/RestaurantPage.java
@@ -6,8 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class RestaurantPage extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RestaurantPage extends AppCompatActivity {
+//    RequestQueue rQueue;
     TextView rTextViewResult;
     TextView restaurantBioTextView;
     String username;
@@ -21,12 +31,13 @@ public class RestaurantPage extends AppCompatActivity {
         restaurantBioTextView = findViewById(R.id.restaurantBioView);
         rTextViewResult = findViewById(R.id.restaurantPost);
 
-        Button refreshButton = findViewById(R.id.refreshButton);
+        Button refreshButton = findViewById(R.id.refreshRestaurantButton);
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getRestaurantProfilePosts();
-                getRestaurantBio();
+             System.out.println("we out here");
+//                getRestaurantProfilePosts();
+//                getRestaurantBio();
             }
         });
 
@@ -34,13 +45,54 @@ public class RestaurantPage extends AppCompatActivity {
 
     }
 
-    public void getRestaurantProfilePosts(){
+//    public void getRestaurantProfilePosts(){
+//
+//    }
+//
+//    public void getRestaurantBio(){
+//        String url = "http://coms-309-mg-1.cs.iastate.edu:8080/photo/all";
+//
+//        JsonArrayRequest bioRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+//            @Override
+//            public void onResponse(JSONArray response) {
+//
+//                try {
+//
+//                    if(response.length() != 0) {
+//
+//                        for (int i = 0; i < response.length(); i++) {
+//                            JSONObject userBio = response.getJSONObject(i);
+//
+//                            String bio = userBio.getString("bio");
+//                            username = userBio.getString("username");
+//
+//
+//
+//                            restaurantBioTextView.setText("\t\tUser Bio \n\t\t-------------\nUser Name : " + username + "\nBio: "  + bio );
+//
+//                        }
+//                    }else{
+//                        restaurantBioTextView.append("No profile");
+//                    }
+//
+//
+//                } catch (JSONException e) {
+//                    restaurantBioTextView.setText("JSON EXCEPTION");
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        }
+//        );
+//
+//        rQueue.add(bioRequest);
+//    }
+//
 
-    }
-
-    public void getRestaurantBio(){
-
-    }
 
 
 }
